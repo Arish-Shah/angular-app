39166f1b36d6bd399228d2ed5227260273c221b2
Arish-Shah <arish0246@gmail.com>
2020-02-02 16:47:44 +0530

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div style="display: inline-block; margin: 0 auto;">
+      <h1>{{ title }}</h1>
+    </div>
+  `
 })
 export class AppComponent {
-  title = 'test';
+  title = 'ng';
 }
