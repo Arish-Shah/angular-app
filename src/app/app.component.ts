150ff3832cafea0ca8e40efbd612fee658fed846
Arish-Shah <arish0246@gmail.com>
2020-05-07 13:36:26 +0530

    add directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  name = 'Arish';
+  newTodo = '';
+  todos = [{ text: 'A first item' }];
+
+  onSubmit(event: Event) {
+    event.preventDefault();
+    if (this.newTodo.trim() !== '') {
+      this.todos.unshift({ text: this.newTodo });
+      this.newTodo = '';
+    }
+  }
 }
