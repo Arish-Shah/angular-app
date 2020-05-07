d4b74303e59b02e0bf03f44e543509ce6e11cc03
Arish-Shah <arish0246@gmail.com>
2020-05-07 15:24:09 +0530

    add template to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  newTodo = '';
-  todos = [{ text: 'A first item' }];
-
-  onSubmit(event: Event) {
-    event.preventDefault();
-    if (this.newTodo.trim() !== '') {
-      this.todos.unshift({ text: this.newTodo });
-      this.newTodo = '';
-    }
-  }
-}
+export class AppComponent {}
