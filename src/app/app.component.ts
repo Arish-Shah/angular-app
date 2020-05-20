7e1bd146528f90db2e4b8ccf675c165833acc0e1
Arish-Shah <arish0246@gmail.com>
2020-05-20 14:01:14 +0530

    add td

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,12 +7,38 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signUpForm.form.patchValue({
+      userData: { username: suggestedName },
+    });
   }
 
   /* Function executes on submitting form */
-  onSubmit(form: NgForm) {
-    console.log(form.value);
+  // onSubmit(form: NgForm) {
+  //   console.log(form.value);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.username = this.signUpForm.value.userData.username;
+    this.user.email = this.signUpForm.value.userData.email;
+    this.user.secretQuestion = this.signUpForm.value.secret;
+    this.user.answer = this.signUpForm.value.questionAnswer;
+    this.user.gender = this.signUpForm.value.gender;
+    this.signUpForm.reset();
   }
 }
