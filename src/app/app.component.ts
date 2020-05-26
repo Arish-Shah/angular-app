72600d9da5595c6e7e43d0d89b7c764b3841d8c1
Arish-Shah <arish0246@gmail.com>
2020-05-26 23:12:40 +0530

    add interceptors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+
 import { Post } from './post.model';
 import { PostsService } from './posts.service';
 
@@ -7,31 +10,65 @@ import { PostsService } from './posts.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  loadedPosts = [];
-  isFetching: boolean;
+export class AppComponent implements OnInit, OnDestroy {
+  loadedPosts: Post[] = [];
+  isFetching = false;
+  error = null;
+  private errorSub: Subscription;
 
   constructor(private postsService: PostsService) {}
 
   ngOnInit() {
-    this.onFetchPosts();
+    this.errorSub = this.postsService.error.subscribe((errorMessage) => {
+      this.error = errorMessage;
+    });
+
+    this.isFetching = true;
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.isFetching = false;
+        this.error = error.message;
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
-    this.postsService.createAndStorePosts(postData.title, postData.content);
+    // Send Http request
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
+    // Send Http request
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe((posts) => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.isFetching = false;
+        this.error = error.message;
+        console.log(error);
+      }
+    );
   }
 
   onClearPosts() {
+    // Send Http request
     this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,40 +1,87 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Post } from './post.model';
-import { map } from 'rxjs/operators';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpParams,
+  HttpEventType,
+} from '@angular/common/http';
+import { map, catchError, tap } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root',
-})
+import { Post } from './post.model';
+
+@Injectable({ providedIn: 'root' })
 export class PostsService {
-  url = 'https://ng-complete-guide-19eb8.firebaseio.com/posts.json';
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) {}
 
-  createAndStorePosts(title, content) {
-    const postData: Post = { title, content };
+  createAndStorePost(title: string, content: string) {
+    const postData: Post = { title: title, content: content };
     this.http
-      .post<{ name: string }>(this.url, postData)
-      .subscribe((response) => {
-        console.log(response);
-      });
+      .post<{ name: string }>(
+        'https://ng-complete-guide-19eb8.firebaseio.com/posts.json',
+        postData,
+        {
+          observe: 'response',
+        }
+      )
+      .subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          this.error.next(error.message);
+        }
+      );
   }
 
   fetchPosts() {
-    return this.http.get<Post[]>(this.url).pipe(
-      map((responseData) => {
-        const postArray: Post[] = [];
-        for (const key in responseData) {
-          if (responseData.hasOwnProperty(key)) {
-            postArray.push({ ...responseData[key], id: key });
-          }
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
+    return this.http
+      .get<{ [key: string]: Post }>(
+        'https://ng-complete-guide-19eb8.firebaseio.com/posts.json',
+        {
+          headers: new HttpHeaders({ 'Custom-Header': 'Hello' }),
+          params: searchParams,
+          responseType: 'json',
         }
-        return postArray;
-      })
-    );
+      )
+      .pipe(
+        map((responseData) => {
+          const postsArray: Post[] = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        }),
+        catchError((errorRes) => {
+          // Send to analytics server
+          return throwError(errorRes);
+        })
+      );
   }
 
   deletePosts() {
-    return this.http.delete(this.url);
+    return this.http
+      .delete('https://ng-complete-guide-19eb8.firebaseio.com/posts.json', {
+        observe: 'events',
+        responseType: 'text',
+      })
+      .pipe(
+        tap((event) => {
+          console.log(event);
+          if (event.type === HttpEventType.Sent) {
+            // ...
+          }
+          if (event.type === HttpEventType.Response) {
+            console.log(event.body);
+          }
+        })
+      );
   }
 }
