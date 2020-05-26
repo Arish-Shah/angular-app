4755e6b393360557ca5c889ea023b7cb2a1889ed
Arish-Shah <arish0246@gmail.com>
2020-05-26 11:59:32 +0530

    add resolver

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { DataStorageService } from '../services/data-storage.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent implements OnInit {
-  constructor() {}
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
 
-  ngOnInit(): void {}
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
+
+  onFetchData() {
+    this.dataStorageService.fetchRecipes().subscribe();
+  }
 }
